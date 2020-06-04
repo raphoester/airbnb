af82bfd09b627af8f1093c59aa52edbc4a45ae64
raphoester <56402078+raphoester@users.noreply.github.com>
2020-06-04 14:24:00 +0200

    Add files via upload

--- recherche.php
+++ recherche.php
@@ -8,7 +8,7 @@ if(!empty($_GET))
     <h2>Resultats de la recherche</h2>
     <p><a href="index.php">Rechercher autre chose ?</a></p>
     <?php 
-    $annoncesCorrespondantes = $pdo->query("SELECT * FROM annonce WHERE ville ="."'".$_GET['ville']."';");
+    $annoncesCorrespondantes = $pdo->query("SELECT * FROM annonce WHERE ville ="."'".$_GET['ville']."' AND locataires_max >= "."'".$_GET['places']."' ;");
     $reservationsConflit = $pdo->query("SELECT * from reservation where (date_debut >= "."'".$_GET['dA']."' AND date_debut <= "."'".$_GET['dD']."') OR (date_fin >= "."'".$_GET['dA']."' AND date_fin <= "."'".$_GET['dD']."');");
     $tout = $annoncesCorrespondantes->fetchAll();
     $conflits = $reservationsConflit->fetchAll();
@@ -36,9 +36,9 @@ if(!empty($_GET))
         {
             ?>
             </div>
-            <a href="<?php echo 'annonce.php?id='.$tout[$i]["id_annonce"]?>"><?php echo $tout[$i]["titre"]?></a>
+            <h3><a href="<?php echo 'annonce.php?id='.$tout[$i]["id_annonce"]?>"><?php echo $tout[$i]["titre"]?></a></h3>
             <h5><?php echo $tout[$i]['ville']?></h5>
-            <h4>RÉSERVÉ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! fallait aller plus vite enculé<br><h4>
+            <h4>RÉSERVÉ !!!!!!!!!!!! fallait aller plus vite mon pote<br><h4>
             </div>
             <?php
         }
