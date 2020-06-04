ee75dec9d948802bda051ebd16d451e883c931f5
raphoester <56402078+raphoester@users.noreply.github.com>
2020-06-04 17:54:56 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,43 +1,58 @@
+<?php 
+    session_start();
+    $pdo = new PDO("mysql:host=localhost; dbname=airbnb", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Airbnb</title>
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+    <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous"></script>
+    <script src="semantic/dist/semantic.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
 </head>
 <body>
-    <header>
-        <ul>
-            <li>
-                <a href="index.php"><img src="img/logo_airbnb.jpg" alt="" width = 100px></a>
-            </li>
-            <li>
-                <ul>
-                <?php 
-                session_start();
-                $pdo = new PDO("mysql:host=localhost;dbname=airbnb", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-                if(empty($_SESSION["login"]))
-                {?>
-                    <li>
-                    <a href="connexion.php">Se connecter</a>
-                    </li>
-                    <li>
-                        <a href="creercompte.php">Créer un compte</a>
-                    </li>
-                <?php
-                }
-                else if ($_SESSION["login"] == 1)
-                {?>
-                    <li>
-                        <a href="nouvelleannonce.php">Poster une annonce</a>
-                    </li>
-                    <li>
-                        <a href="compte.php">Espace personnel</a>
-                    </li>
-                <?php }?>
-                </ul>
-            </li>
+<header>
+    <nav>
+        <ul class="nav justify-content-end">
+        <a href="index.php">
+        <img class="logo" src="img/logo_airbnb1.jpg" alt="logo" href="index.php">
+
+        <?php
+        if(empty($_SESSION["login"]))
+        {?>
+
+        <li class="nav-item">
+            <a class="nav-link" href="connexion.php">Connexion</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="creercompte.php">Inscription</a>
+        </li>
+        <?php 
+        }
+        else if ($_SESSION["login"] == 1)
+        {?>
+
+        <li class="nav-item">
+            <a class="nav-link" href="nouvelleannonce.php">Poster une annonce</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="compte.php">Espace personnel</a>
+        </li>
+        <?php } ?>
         </ul>
-    </header>
+    </nav>
+</header>
+
+
+</html>
--- index.php
+++ index.php
@@ -1,35 +1,50 @@
 <?php include("inc/header.php");?>
 
+<div class="type">
 
 <h1>Partez en vacances.</h1>
 <h3>Mais où ?</h3>
 <h3>Quand ?</h3>
 <h3>Telle est la question.</h3>
 
-<form action="recherche.php", method=GET>
-    <div>
-        <label>Ville</label>
-        <input type="text" placeholder="Où allez-vous ?" name = "ville" required>
-    </div>
-    <div>
-        <label>Arrivée</label>
-        <input type = "date" name = "dA" required>
-    </div>
-    <div>
-        <label>Départ</label>
-        <input type="date" name="dD" required>
-    </div>
-    <div>
-        <label>Nombre de places</label>
-        <input type="number" min="1" max="999" name = "places">
-    </div>
-    <div>
-        <label>Rechercher</label>
-        <input type="submit">
-    </div>
-</form>
+<form id="form" class="recherche" action="recherche.php" method=GET>
+    <div class="ui form">
+        <div class="five fields">
+            <div class="field">
+                <label for="ville">Ville</label>
+                <input type="text" placeholder="Ou aller ?" name = "ville" id="ville" required>
+            </div>
 
+            <div class="field">
+                <label for="date">Date d'arrivée</label>
+                <input type="date" name = "dA" id="date" required>
+            </div>
 
+            <div class="field">
+                <label for="dD">Date de départ</label>
+                <input type="date" name="dD" id="dD" required>
+            </div>
 
+            <div class = "field">
+                <label for="places">Nombre de places</label>
+                <input type="number" min="1" max="999" name = "places" id="places" required>
+            </div>
+
+            <div class="field">
+                <label for="prixMin">Prix minimum</label>
+                <input type="number" name="prixMin" min = 0 max = 100000 id="prixMin">
+            </div>
+
+            <div class="field">
+                <label for="prixMax">Prix maximum</label>
+                <input type="number" name="prixMax" min = 0 max = 100000 id="prixMax">
+            </div>
+
+            <div class="btn_ajt">
+                <button type="submit" form="form" name="button" class="btn btn-light">Rechercher</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include("inc/footer.php");?>
